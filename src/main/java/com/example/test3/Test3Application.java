83e6080de028c63ffa10b6e64a2797c2a2621dd8
guanzhengfu <guanzheng.fu@thoughtworks.com>
2022-04-08 12:38:52 +0800

    test

--- src/main/java/com/example/test3/Test3Application.java
+++ src/main/java/com/example/test3/Test3Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
   public static void main(String[] args) {
-    System.out.println("自动触发test");
+    System.out.println("自动触发test1");
     SpringApplication.run(Test3Application.class, args);
   }
 
