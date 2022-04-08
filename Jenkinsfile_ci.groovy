08ac220936e6aaa16cdd4792f3b95783dd0ada0d
guanzhengfu <guanzheng.fu@thoughtworks.com>
2022-04-08 18:11:02 +0800

    test4

--- Jenkinsfile_ci.groovy
+++ Jenkinsfile_ci.groovy
@@ -6,7 +6,7 @@ pipeline {
                 timeout(time: 1, unit: 'HOURS')
             }
             steps {
-                echo 'Hello World33333'
+                echo 'Hello World44444'
             }
         }
     }
